74f2618cc1f9f6290522a2cc3e7893fa5f4e3bbb
Prasath140497 <prasathcivil140497@gmail.com>
2022-07-09 11:53:49 +0530

    message

---
+++ src/org/login/Login.java
@@ -0,0 +1,17 @@
+package org.login;
+
+public class Login {
+	private void StudentId() {
+		System.out.println("Student ID: 12345");
+	}
+
+	private void StudentName() {
+		System.out.println("Student Name:HARIPRASATH ");
+	}
+
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.StudentId();
+		login.StudentName();
+	}
+}
